fa82c82ffc65928470e73e70310c83fa33476b78
Chanikya <chanikyaprakash@gmail.com>
2019-12-05 20:00:59 +0530

    Added spinner

--- app.js
+++ app.js
@@ -3,21 +3,41 @@
 
 	angular.module('RootScopeDemoApp',[])
 	.controller('AppOneController',AppOneController)
-	.controller('AppTwoController',AppTwoController);
+	.controller('AppTwoController',AppTwoController)
+	.controller('SpinnerController', SpinnerController);
 
-	AppOneController.$inject=['$rootScope'];
+	SpinnerController.$inject=['$rootScope'];
+	function SpinnerController($rootScope){
+		var $ctrl=this;
+		$ctrl.show=false;
+		var spinnerCancelListener=$rootScope.$on('AppOneController:showSpinner',function (event, data) {
+			$ctrl.show=data.showFlag;
+		});
+		$ctrl.$onDestroy = function () {
+			spinnerCancelListener();
+		};
 
-	function AppOneController($rootScope){
+	}
+
+	AppOneController.$inject=['$rootScope','$timeout'];
+	function AppOneController($rootScope,$timeout){
 		var $ctrl=this;
 		$ctrl.Name="";
 
 		$ctrl.sendEvent=function(){
-			console.log("sendEvent");
-			$rootScope.$broadcast('AppOneController.processing',{Name: $ctrl.Name});
+			if ($ctrl.Name == "") {
+				$rootScope.$broadcast('AppOneController:error',{ Name: "Please Enter Name! " });
+			}else{
+				$rootScope.$broadcast('AppOneController:processing',{ Name: "Hello "+$ctrl.Name });
+				$rootScope.$broadcast('AppOneController:showSpinner',{ showFlag: true });
+				$timeout(function() {
+					$rootScope.$broadcast('AppOneController:processing',{ Name: "Bye "+$ctrl.Name });
+					$rootScope.$broadcast('AppOneController:showSpinner',{ showFlag: false });
+				}, 2000);
+			}
 		};
 
 	}//endof appOneController
-
 
 	AppTwoController.$inject=['$rootScope'];
 	function AppTwoController($rootScope) {
@@ -25,22 +45,24 @@
 		$ctrl.userName='';
 
 		var cancelListener = $rootScope.$on('AppOneController:processing', function (event, data) {
-			console.log("Event: ", event);
-			console.log("Data: ", data);
+			// console.log("Event: ", event);
+			// console.log("Data: ", data);
 
-			if (data.Name) {
-				$ctrl.userName = "Hello "+data.Name;
-			}
-			else {
-				$ctrl.showSpinner = "Please enter user name!";
-			}
+			$ctrl.userName = data.Name;
+		});
+
+		var cancelListenerError = $rootScope.$on('AppOneController:error', function (event, data) {
+			// console.log("Event: ", event);
+			// console.log("Data: ", data);
+
+			$ctrl.userName = data.Name;
 		});
 
 		$ctrl.$onDestroy = function () {
 			cancelListener();
+			cancelListenerError();
 		};
 
 	}
-
 
 })();
